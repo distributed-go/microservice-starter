3ab74dec88e9a509d576a925eb65590b375e60d5
Gufran Mirza <gufranmirza1@gmail.com>
2021-05-30 00:22:58 +0530

    update omitempty

--- src/database/dbmodels/jobs.go
+++ src/database/dbmodels/jobs.go
@@ -8,30 +8,30 @@ import (
 
 // Job model represents the job collection in database
 type Job struct {
-	ID                  primitive.ObjectID `json:"id" bson:"_id"`
-	CreatedTimestampUTC time.Time          `json:"createdTimestampUTC" bson:"CreatedTimestampUTC"`
-	UpdatedTimestampUTC time.Time          `json:"updatedTimestampUTC" bson:"UpdatedTimestampUTC"`
+	ID                  primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	CreatedTimestampUTC time.Time          `json:"createdTimestampUTC,omitempty" bson:"CreatedTimestampUTC,omitempty"`
+	UpdatedTimestampUTC time.Time          `json:"updatedTimestampUTC,omitempty" bson:"UpdatedTimestampUTC,omitempty"`
 
-	Title    string                 `json:"title" bson:"Title"`
-	Summary  map[string]interface{} `json:"summary" bson:"Summary"`
-	SideNote string                 `json:"sideNote" bson:"SideNote"`
+	Title    string                 `json:"title,omitempty" bson:"Title,omitempty"`
+	Summary  map[string]interface{} `json:"summary,omitempty" bson:"Summary,omitempty"`
+	SideNote string                 `json:"sideNote,omitempty" bson:"SideNote,omitempty"`
 
-	Locations        []string `json:"locations" bson:"Locations"`
-	MustHaveSkills   []string `json:"mustHaveSkills" bson:"MustHaveSkills"`
-	GoodToHaveSkills []string `json:"goodToHaveSkills" bson:"GoodToHaveSkills"`
-	MinExperience    int      `json:"minExperience" bson:"MinExperience"`
-	MaxExperience    int      `json:"maxExperience" bson:"MaxExperience"`
-	EmploymentType   []string `json:"employmentType" bson:"EmploymentType"`
-	Category         string   `json:"category" bson:"Category"`
-	Function         string   `json:"function" bson:"Function"`
+	Locations        []string `json:"locations,omitempty" bson:"Locations,omitempty"`
+	MustHaveSkills   []string `json:"mustHaveSkills,omitempty" bson:"MustHaveSkills,omitempty"`
+	GoodToHaveSkills []string `json:"goodToHaveSkills,omitempty" bson:"GoodToHaveSkills,omitempty"`
+	MinExperience    int      `json:"minExperience,omitempty" bson:"MinExperience,omitempty"`
+	MaxExperience    int      `json:"maxExperience,omitempty" bson:"MaxExperience,omitempty"`
+	EmploymentType   []string `json:"employmentType,omitempty" bson:"EmploymentType,omitempty"`
+	Category         string   `json:"category,omitempty" bson:"Category,omitempty"`
+	Function         string   `json:"function,omitempty" bson:"Function,omitempty"`
 
-	IsRemote        bool      `json:"isRemote" bson:"IsRemote"`
-	RemoteTimezone  time.Time `json:"remoteTimezone" bson:"RemoteTimezone"`
-	VisaSponsorShip bool      `json:"visaSponsorShip" bson:"VisaSponsorShip"`
+	IsRemote        bool      `json:"isRemote,omitempty" bson:"IsRemote,omitempty"`
+	RemoteTimezone  time.Time `json:"remoteTimezone,omitempty" bson:"RemoteTimezone,omitempty"`
+	VisaSponsorShip bool      `json:"visaSponsorShip,omitempty" bson:"VisaSponsorShip,omitempty"`
 
-	IsVerified bool `json:"isVerified" bson:"IsVerified"`
-	Deleted    bool `json:"deleted" bson:"Deleted"`
+	IsVerified bool `json:"isVerified,omitempty" bson:"IsVerified,omitempty"`
+	Deleted    bool `json:"deleted,omitempty" bson:"Deleted,omitempty"`
 
-	RecruiterID    primitive.ObjectID `json:"recruiterID" bson:"RecruiterID"`
-	OrganizationID primitive.ObjectID `json:"organizationID" bson:"OrganizationID"`
+	RecruiterID    primitive.ObjectID `json:"recruiterID,omitempty" bson:"RecruiterID,omitempty"`
+	OrganizationID primitive.ObjectID `json:"organizationID,omitempty" bson:"OrganizationID,omitempty"`
 }
